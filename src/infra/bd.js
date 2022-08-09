43d26b01cdec9918818c2cd3ca0226313399577d
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-08-09 18:30:32 -0300

    ✨ feat: Banco de dados

---
+++ src/infra/bd.js
@@ -0,0 +1,14 @@
+const bdDicas = [
+    {
+        dica: " Para se livrar do cheiro de alho das mãos, esfregue contra aço inoxidável – sua pia é ideal. Em seguida, lave as mãos com sabão ou detergente."
+    },
+    {
+        dica: " Para remover impressões digitais de utensílios de aço inox, coloque uma pequena quantidade de óleo de bebê em um guardanapo e limpe as áreas afetadas. ."
+    },
+    {
+        dica: " Para deixar o banheiro perfumado, pingue algumas gotinhas da sua essência preferida dentro do rolo do papel higiênico."
+    }
+    
+];
+
+export default bdDicas
